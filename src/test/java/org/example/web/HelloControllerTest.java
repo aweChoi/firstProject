4ada17c135a6e0d57997af71c69a41fa168bf97a
ywchoi <8awe89@gmail.com>
2023-01-08 19:37:15 +0900

    176page 오류로 인해 복구시도

---
+++ src/test/java/org/example/web/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package org.example.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)// 스프링부트 테스트와  JUnit사이의 연결자
+@WebMvcTest(controllers = org.example.web.HelloController.class) //Controller, ControllerAdvice 사용가능
+public class HelloControllerTest {
+    @Autowired //스프링이 관리하는 Bean을 주입받음
+    private MockMvc mvc; //웹 API를 테스트할때 사용, MVC테스트 시작점, 이 클래스를 통해 HTTP GET/POST등에 대한 API테스트 가능
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))// /hello주소로 HTTP GET요청
+                .andExpect(status().isOk())//결과검증(Heaser Status)
+                .andExpect(content().string(hello)); //결과검증 (본문)
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name",name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
